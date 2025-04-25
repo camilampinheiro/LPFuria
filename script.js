dfc44ffdeed9ed4f7482a20a1f81139ec2bc36b8
Camila <camilampinheiro1108@gmail.com>
2025-04-25 12:50:11 -0300

    troquei a fonte

--- script.js
+++ script.js
@@ -27,9 +27,9 @@ function sendMessage() {
     if (msg.includes('jogador') || msg.includes('time')) return 'Jogadores: MOLODOY, YEKINDAR, FalleN, KSCERATO, yuurih. Reservas: skullz e chelo'
     if (msg.includes('loja')) return '🛍️ Acesse a loja oficial: https://www.furia.gg/'
     if (msg.includes('estat')) return '📊 Veja estatísticas em: https://www.hltv.org/team/8297/furia'
-    if (msg.includes('notícia') || msg.includes('novidade')) return 'Últimas notícias em: https://draft5.gg/equipe/330-FURIA'
+    if (msg.includes('notícia') || msg.includes('novidade') || msg.includes('noticia')) return 'Últimas notícias em: https://draft5.gg/equipe/330-FURIA'
     if (msg.includes('clipe') || msg.includes('jogada')) return 'Veja clipes: TWITCH da Furia https://www.twitch.tv/furiatv?lang=pt-br ou YouTube da FURIA https://www.youtube.com/@FURIAggCS'
-    if (msg.includes('história') || msg.includes('sobre')) return 'A FURIA foi fundada em 2017 e é um dos maiores times brasileiros de CS:GO!'
+    if (msg.includes('história') || msg.includes('sobre') || msg.includes('historia')) return 'A FURIA foi fundada em 2017 e é um dos maiores times brasileiros de CS:GO!'
     if (msg.includes('redes') || msg.includes('social')) return 'Redes sociais: Instagram @furiagg, TikTok @furia'
     return '❓ Desculpe, não entendi. Tente: "próximo jogo", "jogadores", "loja", "estatísticas", "notícias", "clipes", "história" ou "redes sociais".'
   }
